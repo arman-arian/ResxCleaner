438088dc100e8ee9f00043de458caaec50f66d38
arman_arian <arman.hassanpoor2000@gmail.com>
2016-06-29 20:28:04 +0430

    update AssemblyInfo and Project Properties

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RESX Unused Finder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ResxCleaner")]
+[assembly: AssemblyDescription("Developed by @arman_arian")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RESX Unused Finder")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyProduct("ResxCleaner")]
+[assembly: AssemblyCopyright("Copyright © @arman_arian 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: GuidAttribute("4D109B8E-8834-4B02-B402-A44824B8C3EE")]
